b49c10070d3649a75183f9ea8d392d99c89ad7ed
dzulfikarubaid <dzulfikarubaid@gmail.com>
2024-06-02 11:58:09 +0700

    update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,7 +8,7 @@ const NotFound = () => {
     <div className='flex flex-col gap-3'>
     <h1 className='text-blue-900 font-semibold'>404 Error</h1>
     <h1 className='text-5xl font-bold'>Page Not Found</h1>
-    <p>Sorry, the page you are looking for doesn't exist. Here are some helpful links:</p>
+    <p>Sorry, the page you are looking for doesn&apos;t exist. Here are some helpful links:</p>
     <div className='flex flex-row gap-4'>
     <button className='border-blue-900 p-4 py-2 border-[1px] flex items-center flex-row gap-2 rounded-lg' onClick={() => window.history.back()}> <BiArrowBack></BiArrowBack><h1>Go Back</h1> </button>
     <Link className='bg-blue-900 text-white p-4 py-2 rounded-lg' href={"/home"}>Take me home</Link>
